42241f851d9ca7f9ae05d9347c0737ed56ddf755
KHAI PHAN <hphan16@my.bcit.ca>
2024-11-05 23:10:29 -0800

    add JS function to store user info in firestore

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -4,10 +4,23 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var uiConfig = {
     callbacks: {
         signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-            // User successfully signed in.
-            // Return type determines whether we continue the redirect automatically
-            // or whether we leave that to developer to handle.
-            return true;
+            var user = authResult.user;
+            if (authResult.additionalUserInfo.isNewUser) {
+                db.collection("users").doc(user.uid).set({
+                    name: user.displayName,
+                    email: user.email,
+                    city: "Vancouver",
+                    country: "Canada",
+                }). then(function() {
+                    console.log("New user added to firestore");
+                    window.location.assign("main.html");
+                }).catch(function(error) {
+                    console.log("Error adding new user: " + error);
+                });
+            } else {
+                return true;
+            }
+                return false;     
         },
         uiShown: function () {
             // The widget is rendered.
