bb6fee9d86751dbeb150a4fbdd19fbad50705b23
KHAI PHAN <hphan16@my.bcit.ca>
2024-11-06 11:54:40 -0800

    Edit JS code for writeInspectionDetails function

--- scripts/home.js
+++ scripts/home.js
@@ -40,7 +40,7 @@ function writeInspectionDetails() {
         check_room_is_quiet: true,
         check_shower_is_hot: true,
         check_kitchen_is_clean: true,
-        completion_date: firebase.firestore.Timestamp.fromDate(new Date("Novvember 2, 2024"))
+        completion_date: firebase.firestore.FieldValue.serverTimestamp()
     });
 
     inspectDetails.add({
@@ -51,7 +51,7 @@ function writeInspectionDetails() {
         check_room_is_quiet: true,
         check_shower_is_hot: true,
         check_kitchen_is_clean: true,
-        creation_date: firebase.firestore.FieldValue.serverTimestamp(),
+        creation_date: firebase.firestore.Timestamp.fromDate(new Date("September 4, 2024")),
         general_inspection_requirements_description: "Is there a bathtub?",
         general_inspector_comments: "No bathtub",
         inspection_is_finished: true,
@@ -72,7 +72,7 @@ function writeInspectionDetails() {
         check_room_is_quiet: null,
         check_shower_is_hot: true,
         check_kitchen_is_clean: true,
-        creation_date: firebase.firestore.FieldValue.serverTimestamp(),
+        creation_date: firebase.firestore.Timestamp.fromDate(new Date("March 3, 2024")),
         general_inspection_requirements_description: "Check kitchen carefully",
         general_inspector_comments: "Kitchen is a bit messy",
         inspection_is_finished: true,
@@ -93,9 +93,9 @@ function writeInspectionDetails() {
         check_room_is_quiet: null,
         check_shower_is_hot: true,
         check_kitchen_is_clean: true,
-        creation_date: firebase.firestore.FieldValue.serverTimestamp(),
+        creation_date: firebase.firestore.Timestamp.fromDate(new Date("May 16, 2024")),
         general_inspection_requirements_description: null,
-        general_inspector_comments: "Bedroom is large",
+        general_inspector_comments: "Bedroom is about 300 sq feet",
         inspection_is_finished: true,
         inspection_rating: 5,
         internet_is_fast: true,
@@ -107,4 +107,4 @@ function writeInspectionDetails() {
     });
 }
 
-writeInspectionDetails()
+// writeInspectionDetails();
