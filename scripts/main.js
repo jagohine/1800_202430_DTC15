2ead1269e3bb893fdecefd4575aa6d5cbfe3c16f
Zoey Jiang <zoeyj8170@gmail.com>
2024-12-03 21:33:59 -0800

    Fix a console error

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,16 @@
 function insertNameFromFirestore() {
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            console.log(user.uid);
-            currentUser = db.collection("users").doc(user.uid);
-            currentUser.get().then(userDoc => {
-                let userName = userDoc.data().name;
-                console.log(userName);
-                document.getElementById("name-goes-here").innerText = userName;
-            })
-        } else {
-            console.log("No user is logged in.")
-        }           
-    })
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      console.log(user.uid);
+      currentUser = db.collection("users").doc(user.uid);
+      currentUser.get().then((userDoc) => {
+        let userName = userDoc.data().name;
+        console.log(userName);
+      });
+    } else {
+      console.log("No user is logged in.");
+    }
+  });
 }
 
 insertNameFromFirestore();
